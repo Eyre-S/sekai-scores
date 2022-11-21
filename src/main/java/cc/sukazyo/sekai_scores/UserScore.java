28ff69ceb8414bfa35dbd2f6a137124db702fe0d
Eyre_S <sukazyo@outlook.com>
2022-11-21 15:06:08 +0800

    add playtime field to UserScore

--- src/main/java/cc/sukazyo/sekai_scores/UserScore.java
+++ src/main/java/cc/sukazyo/sekai_scores/UserScore.java
@@ -1,9 +1,11 @@
 package cc.sukazyo.sekai_scores;
 
 import javax.annotation.Nonnull;
+import java.time.LocalDateTime;
 
 public record UserScore (
 		@Nonnull Song song,
 		@Nonnull Difficulty difficulty,
-		@Nonnull ScoreBase score
+		@Nonnull ScoreBase score,
+		@Nonnull LocalDateTime playtime
 ) {}
