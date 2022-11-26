0ad227ac8e6e4eeb9bf3dea2041f6674b1d7685f
Eyre_S <sukazyo@outlook.com>
2022-11-26 21:49:58 +0800

    add parse for --user param in add command

--- sekai-cli/src/main/java/cc/sukazyo/sekai_cli/client/AddScore.java
+++ sekai-cli/src/main/java/cc/sukazyo/sekai_cli/client/AddScore.java
@@ -2,16 +2,44 @@ package cc.sukazyo.sekai_cli.client;
 
 import cc.sukazyo.sekai_scores.ScoreBase;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import static cc.sukazyo.sekai_cli.Log._debug;
+import static cc.sukazyo.sekai_cli.Log._user;
 
 public class AddScore {
 	
 	public static void main (String[] args) {
 		
+		// process params available in add command
+		Long user_sekai_id = null;
+		final List<String> $args = new ArrayList<>(List.of(args));
+		int $arg_i;
+		if (($arg_i = $args.indexOf("--user")) != -1) {
+			// subcommand --user to use a user instead of the user.id defined in Config
+			$args.remove($arg_i);
+			try {
+				user_sekai_id = Long.parseLong($args.get($arg_i));
+				$args.remove($arg_i);
+				_debug("add: defined user id to use by param: " + user_sekai_id);
+			}
+			catch (IndexOutOfBoundsException e) {
+				_user("add: param --user need a number as user_id.\n  use `--user <user_id>` to make it work properly.");
+				_debug(e);
+			} catch (NumberFormatException e) {
+				_user("add: param --user need a user_id which type should be a number:\n  " + e.getMessage());
+				_debug(e);
+			}
+		}
+		args = $args.toArray(String[]::new);
+		
 		try {
+			
+			///--
+			// scores input
 			
 			final int[] scores = new int[9];
 			
@@ -37,6 +65,10 @@ public class AddScore {
 					scores[0], scores[1], scores[2], scores[3], scores[4],
 					scores[6], scores[5], scores[7], scores[8]
 			);
+			
+			///--
+			// echo and making confirm
+			
 			
 			System.out.println(PrintScore.printTableScoreBase(scoreBase));
 			
